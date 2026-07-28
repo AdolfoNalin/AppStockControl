e2a9531355b07ee52f860811b7e3b493151d55f5
Adolfo Nalin <nalinadolfo33@gmai.com>
2026-07-28 15:28:38 -0300

    Create the object product

---
+++ Models/Product.cs
@@ -0,0 +1,35 @@
+﻿namespace AppStockControl.Models
+{
+    public enum UnitType
+    {
+        Unit,
+        Kg,
+        Liter,
+        Meter,
+        Box,
+        Package
+    }
+
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public Guid SupplierId { get; set; }
+        public Guid CategoryId { get; set; }
+        public int InternalCode { get; set; }
+        public string Description { get; set; }
+        public int StockQuantity { get; set; }
+        public int MinimumStock { get; set; }
+        public int MaximumStock { get; set; }
+        public decimal BuyPrice{ get; set; }
+        public decimal SalePrice{ get; set; }
+        public UnitType UnitType { get; set; }
+        public string? Barcode{ get; set; }
+        public decimal ProfitMargin { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public string? ImagePath { get; set; }
+        public Guid BrandId { get; set; }
+        public string? Observation { get; set; }
+    }
+}
