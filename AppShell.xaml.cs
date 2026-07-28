238571cb834430081a36609a192b3e8848b519f5
Adolfo Nalin <nalinadolfo33@gmai.com>
2026-07-28 16:09:32 -0300

    Create the Architecture

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AppStockControl
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace AppStockControl
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
