d3ec0ef4437a95222454d9ba79dae7129104e309
Adolfo Nalin <nalinadolfo33@gmai.com>
2026-07-28 15:31:30 -0300

    Create the object Brand

---
+++ Models/Brand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+using System.Text.Json.Serialization;
+using Windows.System;
+
+namespace AppStockControl.Models
+{
+    public class Brand
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public bool Active { get; set; } = true;
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
