a5487a2bda2ac76c7fb7387dd0fa467b95c7eb02
Adolfo Nalin <nalinadolfo33@gmai.com>
2026-07-28 15:45:35 -0300

    Create the object User

---
+++ Models/User.cs
@@ -0,0 +1,13 @@
+﻿namespace AppStockControl.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public bool Active { get; set; } = true;
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
