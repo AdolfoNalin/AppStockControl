c87dc25a2ca9e2b89c31987068f4f9e751867136
Adolfo Nalin <nalinadolfo33@gmai.com>
2026-07-28 15:36:59 -0300

    Create the object StockMovement

---
+++ Models/StockMovement.cs
@@ -0,0 +1,22 @@
+﻿namespace AppStockControl.Models
+{
+    public enum MovimentType
+    {
+        Entry,
+        Exit,
+        Ajustment
+    }
+
+    public class StockMovement
+    {
+        public Guid Id { get; set; }
+        public Guid ProductId { get; set; }
+        public Guid UserId { get; set; }
+        public MovimentType MovimentType { get; set; }
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal TotalValue { get; }
+        public string? Observation { get; set; }
+        public DateTime MovementDate { get; set; }
+    }
+}
