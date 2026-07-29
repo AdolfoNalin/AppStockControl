c799803c1d917dba2362aa7b65b6bb10395b59f7
Adolfo Nalin <nalinadolfo33@gmai.com>
2026-07-29 20:24:10 -0300

    Create Class ProducService

---
+++ Service/ProductService.cs
@@ -0,0 +1,217 @@
+﻿using AppStockControl.ConnectionFactore;
+using AppStockControl.Models;
+using AppStockControl.Service.Interfaces;
+using Newtonsoft.Json;
+using System.Collections.ObjectModel;
+using System.Net.Http.Json;
+
+namespace AppStockControl.Service
+{
+    public class ProductService 
+    {
+        #region GetAll
+        public static async Task<ObservableCollection<Product>> GetAll()
+        {
+            try
+            {
+                HttpClient client = ConnectionFactore.ConnectionLocalhost.ConnectionPostgree();
+                HttpResponseMessage response = await client.GetAsync("Product");
+
+                if(response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    ObservableCollection<Product> collections = new ObservableCollection<Product>();
+                    List<Product> products = JsonConvert.DeserializeObject<List<Product>>(await response.Content.ReadAsStringAsync());
+                    products.ForEach(p =>collections.Add(p));
+                    return collections;
+                }
+                else
+                {
+                    throw new ArgumentNullException(await response.Content.ReadAsStringAsync());
+                }
+            }
+            catch(ArgumentNullException ane)
+            {
+                throw ane;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        #endregion
+
+        #region GetById
+        public static async Task<Product> GetById(Guid id)
+        {
+            try
+            {
+                HttpClient client = ConnectionFactore.ConnectionLocalhost.ConnectionPostgree();
+                HttpResponseMessage response = await client.GetAsync($"Product/ById/{id}");
+
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    Product product = JsonConvert.DeserializeObject<Product>(await response.Content.ReadAsStringAsync());
+                    return product;
+                }
+                else
+                {
+                    throw new ArgumentNullException(await response.Content.ReadAsStringAsync());
+                }
+            }
+            catch (ArgumentNullException ane)
+            {
+                throw ane;
+            }
+            catch(ArgumentException ae)
+            {
+                throw ae;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        #endregion
+
+        #region ChangeStatus
+        public static async Task<string> ChangeStatus(Guid id)
+        {
+            try
+            {
+                HttpClient client = ConnectionLocalhost.ConnectionPostgree();
+                HttpResponseMessage response = await client.PutAsync($"Product/ChangeStatus/{id}", null);
+
+                if(response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    string message = await response.Content.ReadAsStringAsync();
+                    return message;
+                }
+                else
+                {
+                    throw new ArgumentNullException(await response.Content.ReadAsStringAsync());
+                }
+            }
+            catch(ArgumentNullException ane)
+            {
+                throw ane;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        #endregion
+
+        #region Create
+        public static async Task<string> Create(Product product)
+        {
+            try
+            {
+                HttpClient client = ConnectionLocalhost.ConnectionPostgree();
+                HttpResponseMessage response = await client.PostAsJsonAsync("Product", product);
+
+                if(response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    string message = await response.Content.ReadAsStringAsync();
+                    return message;
+                }
+                else
+                {
+                    throw new ArgumentNullException(await response.Content.ReadAsStringAsync());
+                }
+            }
+            catch(ArgumentNullException ae)
+            {
+                throw ae;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        #endregion
+
+        #region GetByStatus
+        public async static Task<ObservableCollection<Product>> GetByStatus(bool value)
+        {
+            try
+            {
+                HttpClient client = ConnectionFactore.ConnectionLocalhost.ConnectionPostgree();
+                HttpResponseMessage response = await client.GetAsync($"Product/ByStatus/{value}");
+
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    ObservableCollection<Product> collections = new ObservableCollection<Product>();
+                    List<Product> products = JsonConvert.DeserializeObject<List<Product>>(await response.Content.ReadAsStringAsync());
+                    products.ForEach(p => collections.Add(p));
+                    return collections;
+                }
+                else
+                {
+                    throw new ArgumentNullException(await response.Content.ReadAsStringAsync());
+                }
+            }
+            catch (ArgumentNullException ane)
+            {
+                throw ane;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        #endregion
+
+        #region Update
+        public async static Task<string> Update(Product product)
+        {
+            try
+            {
+                HttpClient client = ConnectionLocalhost.ConnectionPostgree();
+                HttpResponseMessage response = await client.PutAsJsonAsync("Product", product);
+
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    string message = await response.Content.ReadAsStringAsync();
+                    return message;
+                }
+                else
+                {
+                    throw new ArgumentNullException(await response.Content.ReadAsStringAsync());
+                }
+            }
+            catch (ArgumentNullException ae)
+            {
+                throw ae;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        #endregion
+
+        #region UpdateStock
+        public async static Task<bool> UpdateStock(Guid productId, int stockQuantity)
+        {
+            try
+            {
+                HttpClient client = ConnectionLocalhost.ConnectionPostgree();
+                HttpResponseMessage response = await client.PutAsync($"Product/UpdateStock?id={productId}&stockQuantity={stockQuantity}", null);
+
+                bool result = JsonConvert.DeserializeObject<bool>(await response.Content.ReadAsStringAsync());
+
+                return result;
+            }
+            catch (ArgumentNullException ae)
+            {
+                throw ae;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        #endregion
+    }
+}
