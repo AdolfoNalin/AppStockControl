a6a97eb883bea7f95ef545f84a7d118a705c74ac
Adolfo Nalin <nalinadolfo33@gmai.com>
2026-08-11 11:27:27 -0300

    Create method RegisterViews

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using AppStockControl.Views;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 namespace AppStockControl
 {
     public static class MauiProgram
     {
+        public static string endPointAPI = "";
         public static MauiApp CreateMauiApp()
         {
             var builder = MauiApp.CreateBuilder();
@@ -13,13 +16,49 @@ namespace AppStockControl
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-                });
+                }).RegisterViews();
 
 #if DEBUG
     		builder.Logging.AddDebug();
 #endif
 
+
+
+            using var stream = FileSystem.OpenAppPackageFileAsync("appsettings.json").GetAwaiter().GetResult();
+
+            var config = new ConfigurationBuilder().AddJsonStream(stream).Build();
+
+            builder.Configuration.AddConfiguration(config);
+
+            endPointAPI = config["StockControlApp:Endpoint"];
+
             return builder.Build();
         }
+
+        #region RegisterViews
+        public static MauiAppBuilder RegisterViews(this MauiAppBuilder mauiAppBuilder)
+        {
+            try
+            {
+                mauiAppBuilder.Services.AddTransient<Supplier>();
+                mauiAppBuilder.Services.AddTransient<Brand>();
+                mauiAppBuilder.Services.AddTransient<Category>();
+                mauiAppBuilder.Services.AddTransient<Login>();
+                mauiAppBuilder.Services.AddTransient<SignUp>();
+                mauiAppBuilder.Services.AddTransient<Menu>();
+                mauiAppBuilder.Services.AddTransient<ProductAdd>();
+                mauiAppBuilder.Services.AddTransient<ProductList>();
+                mauiAppBuilder.Services.AddTransient<AppShell>();
+                mauiAppBuilder.Services.AddTransient<App>();
+
+                return mauiAppBuilder;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        #endregion
     }
 }
