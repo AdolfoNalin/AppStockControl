cb9874e5b3a41255bd0bd896afecb81ee5e006cf
Adolfo Nalin <nalinadolfo33@gmai.com>
2026-08-12 20:49:11 -0300

    Create class AuthService
    Class responsible for authenticate the user

---
+++ Service/AuthService.cs
@@ -0,0 +1,39 @@
+﻿using AppStockControl.DTOs;
+using AppStockControl.Models;
+
+namespace AppStockControl.Service
+{
+    public class AuthService
+    {
+        public static async Task<bool> AuthUser(Guid id)
+        {
+			try
+			{
+				User user = await UserService.GetById(id);
+				LoginRequest loginRequest = new LoginRequest()
+				{
+					Login = user.Name,
+					PasswordHash = user.PasswordHash,
+				};
+
+				UserResponse response = await UserService.Login(loginRequest);
+				if (response == null)
+				{
+					return false;
+				}
+				else
+				{
+					UserSession.Id = response.Id;
+					UserSession.Login = response.Login;
+					UserSession.Token = response.Token;
+
+					return true;
+				}
+			}
+			catch (Exception ex)
+			{
+				throw ex;
+			}
+        }
+    }
+}
