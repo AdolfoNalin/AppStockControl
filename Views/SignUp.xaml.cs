8648bbb884cf5c31fedade8dd5352f5471cd4c99
Adolfo Nalin <nalinadolfo33@gmai.com>
2026-08-11 21:41:01 -0300

    Programming the button save

--- Views/SignUp.xaml.cs
+++ Views/SignUp.xaml.cs
@@ -1,3 +1,7 @@
+using AppStockControl.Helpers;
+using AppStockControl.Models;
+using AppStockControl.Service;
+
 namespace AppStockControl.Views;
 
 public partial class SignUp : ContentPage
@@ -6,4 +10,38 @@ public partial class SignUp : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    #region Button_Clicked_SaveUser
+    private async void Button_Clicked_SaveUser(object sender, EventArgs e)
+    {
+		try
+		{
+			if(txtPassword.Text.Equals(txtPasswordConfirmation.Text))
+			{
+				User user = new User()
+				{
+					Name = txtLogin.Text,
+					Email = txtEmail.Text,
+					PasswordHash = txtPassword.Text,
+					Active = rbEnable.IsChecked == true ? true : false,
+					CreatedAt = DateTime.UtcNow
+				};
+
+				string message = await UserService.Create(user);
+
+				DisplayAlert("", message, "Fechar");
+
+				Navigation.PopAsync();
+			}
+			else
+			{
+				DisplayAlert("Erro", "Senhas não estão iguais", "Fechar");
+			}
+		}
+		catch (Exception ex)
+		{
+			DisplayAlert("Erro", MessageException.Message(ex), "Fechar");
+		}
+    }
+    #endregion
 }
