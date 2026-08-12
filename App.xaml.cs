0622946176a12ba9a26d38c7225f0c58503116c1
Adolfo Nalin <nalinadolfo33@gmai.com>
2026-08-12 20:54:47 -0300

    Create programming logic for authenticate the user

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,13 +1,30 @@
-﻿using AppStockControl.Views;
+﻿using AppStockControl.Models;
+using AppStockControl.Service;
+using AppStockControl.Views;
 
 namespace AppStockControl
 {
     public partial class App : Application
     {
-        public App(Login login)
+        public App()
         {
-            MainPage = login;
-            InitializeComponent();
+            try
+            {
+                InitializeComponent();
+                MainPage = new SplashPage();
+                _ = InitializeAsync();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        private async Task InitializeAsync()
+        {
+            Guid userId = Guid.Parse(await SecureStorage.GetAsync("userId"));
+            bool restored = await AuthService.AuthUser(userId);
+            MainPage = restored ? new AppShell() : new Login();
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
