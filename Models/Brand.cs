46f8fce5d488373acbb34676ee8fb80a223eacaa
Adolfo Nalin <nalinadolfo33@gmai.com>
2026-08-15 21:28:05 -0300

    Setting the CreatedAt for UtcNow

--- Models/Brand.cs
+++ Models/Brand.cs
@@ -7,7 +7,7 @@
         public string Name { get; set; }
         public string? Description { get; set; }
         public bool Active { get; set; } = true;
-        public DateTime CreatedAt { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
         public DateTime? UpdatedAt { get; set; }
     }
 }
