df6319d8d4ab6ddc782d58d91e51bea1c9dc1002
Adolfo Nalin <nalinadolfo33@gmai.com>
2026-08-20 19:50:21 -0300

    Create class responsible for search the zipCode

---
+++ Helpers/ValidationCEP.cs
@@ -0,0 +1,44 @@
+﻿using AppStockControl.Models;
+using System.Data;
+
+namespace AppStockControl.Helpers
+{
+    public class ValidationCEP
+    {
+        public static Address GetCEP(string cep)
+        {
+			try
+			{
+				string xmlUrl = "https://viacep.com.br/ws/" + cep + "/xml";
+				DataSet ds = new DataSet();
+				ds.ReadXml(xmlUrl);
+				ds.ReadXml(xmlUrl);
+
+				if (ds.Tables[0].Equals("erro"))
+				{
+					throw new ArgumentNullException($"CEP {cep} inválido");
+				}
+				else
+				{
+					Address address = new Address()
+					{
+						State = ds.Tables[0].Rows[0]["uf"].ToString(),
+						City = ds.Tables[0].Rows[0]["localidade"].ToString(),
+						District = ds.Tables[0].Rows[0]["bairro"].ToString(),
+						Street = ds.Tables[0].Rows[0]["logradouro"].ToString(),
+					};
+
+					return address;
+				}
+            }
+			catch(ArgumentNullException ane)
+			{
+				throw ane;
+			}
+			catch (Exception ex)
+			{
+				throw ex;
+			}
+        }
+    }
+}
