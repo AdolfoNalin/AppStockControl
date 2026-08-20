66411afe4fbb3ce6eb5c0e21c60a086bb9fc6ea0
Adolfo Nalin <nalinadolfo33@gmai.com>
2026-08-20 20:17:35 -0300

    OrderBy Attributes

--- Models/Address.cs
+++ Models/Address.cs
@@ -2,12 +2,13 @@
 {
     public class Address
     {
+        public Guid Id { get; set; }
+        public string ZipCode { get; set; } = string.Empty;
+        public string State { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public string District { get; set; } = string.Empty;
         public string Street { get; set; } = string.Empty;
         public string Number { get; set; } = string.Empty;
         public string Complement { get; set; } = string.Empty;
-        public string District { get; set; } = string.Empty;
-        public string City { get; set; } = string.Empty;
-        public string State { get; set; } = string.Empty;
-        public string ZipCode { get; set; } = string.Empty;
     }
 }
