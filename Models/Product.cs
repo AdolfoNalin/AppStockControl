44c55fe06623ff1b865de45000d979aaff7064c3
Adolfo Nalin <nalinadolfo33@gmai.com>
2026-08-25 06:43:39 -0300

    Removing the internalCode
    Adding calculation ProfitMargin

--- Models/Product.cs
+++ Models/Product.cs
@@ -13,10 +13,10 @@
     public class Product
     {
         public Guid Id { get; set; }
+        public Guid BrandId { get; set; }
         public Guid UserId { get; set; }
         public Guid SupplierId { get; set; }
         public Guid CategoryId { get; set; }
-        public int InternalCode { get; set; }
         public string Description { get; set; }
         public int StockQuantity { get; set; }
         public int MinimumStock { get; set; }
@@ -25,11 +25,13 @@
         public decimal SalePrice{ get; set; }
         public UnitType UnitType { get; set; }
         public string? Barcode{ get; set; }
-        public decimal ProfitMargin { get; }
+        public decimal ProfitMargin =>
+        BuyPrice == 0
+        ? 0
+        : ((SalePrice - BuyPrice) / BuyPrice) * 100;
         public DateTime CreatedAt { get; set; }
         public DateTime? UpdatedAt { get; set; }
         public string? ImagePath { get; set; }
-        public Guid BrandId { get; set; }
         public string? Observation { get; set; }
     }
 }
